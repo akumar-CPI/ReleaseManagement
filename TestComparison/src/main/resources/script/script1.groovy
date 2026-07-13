03a8fabde7cb74ab563557e62c32741448616bda
akumar-CPI <akumar.sap.cpi@gmail.com>
2026-07-13 09:07:45 +0530

    Add files via upload

---
+++ TestComparison/src/main/resources/script/script1.groovy
@@ -0,0 +1,35 @@
+import com.sap.gateway.ip.core.customdev.util.Message
+import groovy.xml.*
+
+def Message processData(Message message) {
+
+    def body = message.getBody(String)
+
+    def employees = new XmlSlurper().parseText(body)
+
+    def writer = new StringWriter()
+    def xml = new MarkupBuilder(writer)
+
+    xml.DepartmentEmployees {
+
+        employees.Employee.groupBy { it.DeptCode.text() }.each { dept, empList ->
+
+            Department(name: dept) {
+
+                empList.each { emp ->
+
+                    Employee {
+                        EmpNumber(emp.EmpNumber.text())
+                        FullName(emp.FullName.text())
+                        ContactEmail(emp.ContactEmail.text())
+                        AnnualSalary(emp.AnnualSalary.text())
+                        Status(emp.Status.text())
+                    }
+                }
+            }
+        }
+    }
+
+    message.setBody(writer.toString())
+    return message
+}
